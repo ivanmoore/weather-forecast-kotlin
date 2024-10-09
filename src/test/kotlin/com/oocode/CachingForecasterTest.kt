a390ff3b1c9b8b738ba40817b7f06ada959d4faa
Ivan Moore <ivan@teamoptimization.com>
2024-10-09 17:38:13 +0100

    added first CachingForecasterTest

--- src/test/kotlin/com/oocode/CachingForecasterTest.kt
+++ src/test/kotlin/com/oocode/CachingForecasterTest.kt
@@ -1,11 +1,21 @@
 package com.oocode
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import java.time.DayOfWeek
 
 class CachingForecasterTest {
     @Test
-    fun `what would be a good first test`() {
+    fun `delegates if never been asked for forecast before`() {
         val fakeForecaster = FakeForecaster()
+        val preCannedForecast = Forecast(1, 2, "cold")
+        fakeForecaster.preCannedResponses[DayOfWeek.SATURDAY to "Here East"] = preCannedForecast
+
         val forecaster = CachingForecaster(fakeForecaster)
+        val actualForecast = forecaster.forecast(DayOfWeek.SATURDAY, "Here East")
+
+        assertThat(actualForecast, equalTo(preCannedForecast))
+        assertThat(fakeForecaster.calledWith, equalTo(listOf(DayOfWeek.SATURDAY to "Here East")))
     }
 }
